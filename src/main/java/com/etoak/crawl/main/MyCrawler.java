f96c5c35c4a4a68010b29d2da594eccb21ef2a69
yuecuiwu <1269849344@qq.com>
2019-06-30 12:58:27 +0800

    提交文件

---
+++ src/main/java/com/etoak/crawl/main/MyCrawler.java
@@ -0,0 +1,107 @@
+package com.etoak.crawl.main;
+
+import com.etoak.crawl.link.LinkFilter;
+import com.etoak.crawl.link.Links;
+import com.etoak.crawl.page.Page;
+import com.etoak.crawl.page.PageParserTool;
+import com.etoak.crawl.page.RequestAndResponseTool;
+import com.etoak.crawl.util.FileTool;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jsoup.select.Elements;
+
+import java.util.Set;
+
+public class MyCrawler {
+
+    /**
+     * 使用种子初始化 URL 队列
+     *
+     * @param seeds 种子 URL
+     * @return
+     */
+    private void initCrawlerWithSeeds(String[] seeds) {
+        for (int i = 0; i < seeds.length; i++){
+            Links.addUnvisitedUrlQueue(seeds[i]);
+        }
+    }
+
+
+    /**
+     * 抓取过程
+     *
+     * @param seeds
+     * @return
+     */
+    public void crawling(String[] seeds) {
+    	
+    	Log log = LogFactory.getLog(MyCrawler.class);
+    	
+        //初始化 URL 队列
+        initCrawlerWithSeeds(seeds);
+
+        //定义过滤器，提取以 http://www.baidu.com 开头的链接
+//        LinkFilter filter = new LinkFilter() {
+//            public boolean accept(String url) {
+//                if (url.startsWith("http://www.baidu.com"))
+//                    return true;
+//                else
+//                    return false;
+//            }
+//        };
+
+        //循环条件：待抓取的链接不空且抓取的网页不多于 1000
+        while (!Links.unVisitedUrlQueueIsEmpty()  && Links.getVisitedUrlNum() <= 1000) {
+
+            //先从待访问的序列中取出第一个；
+            String visitUrl = (String) Links.removeHeadOfUnVisitedUrlQueue();
+            if (visitUrl == null){
+                continue;
+            }
+
+            //根据URL得到page;
+            Page page = RequestAndResponseTool.sendRequstAndGetResponse(visitUrl);
+
+            //对page进行处理： 访问DOM的某个标签
+            Elements es = PageParserTool.select(page,"a");
+            if(!es.isEmpty()){
+                System.out.println("下面将打印所有a标签： ");
+                log.info("下面将打印所有a标签： "+ es);
+                System.out.println(es);
+            }
+
+            //将保存文件
+            FileTool.saveToLocal(page);
+
+            //将已经访问过的链接放入已访问的链接中；
+            Links.addVisitedUrlSet(visitUrl);
+
+            //得到超链接
+            Set<String> links = PageParserTool.getLinks(page,"img");
+            for (String link : links) {
+                Links.addUnvisitedUrlQueue(link);
+                log.info("新增爬取路径: " + link);
+                System.out.println("新增爬取路径: " + link);
+            }
+        }
+    }
+
+
+//    //main 方法入口
+//    public static void main(String[] args) {
+//        MyCrawler crawler = new MyCrawler();
+//        //crawler.crawling(new String[]{"https://www.baidu.com"});
+//        crawler.crawling(new String[]{"http://www.0319xx.com/2536_10.html"});
+//    }
+    
+
+    //main 方法入口
+    public static void main(String[] args) {
+    		MyCrawler crawler = new MyCrawler();
+    	crawler.crawling(new String[]{"http://image.baidu.com/search/index?tn=baiduimage&ct=201326592&lm=-1&cl=2&ie=gb18030&word=%CD%BC%C6%AC&fr=ala&ala=1&alatpl=others&pos=0"});
+    	 data-objurl="http://pic37.nipic.com/20140113/8800276_184927469000_2.png"
+    			 data-thumburl="http://img5.imgtn.bdimg.com/it/u=3300305952,1328708913&amp;fm=26&amp;gp=0.jpg" 
+     
+    }
+}
